bad2fb5872ddf77344fd9c2cdea6af76beb77ff6
Jackson Kujur <95626883+00jackson@users.noreply.github.com>
2023-05-30 22:18:39 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const AboutMe = () => {
                 Hello! I'm a <strong>frontend developer</strong> based in India, with a <strong>passion for creating beautiful and functional</strong> websites. Moreover, I'm a <strong>passionate leader</strong> who loves to hit the <strong>gym</strong> and maintain a clean diet. With a positive attitude, I'm always ready to <strong>explore</strong> new places and learn new technologies. <strong>Consistent</strong> coding practice is my forte, and I welcome every <strong>new tech stack</strong> with open arms.
             </p>
             <div className="flex justify-center">
-                <a href="https://drive.google.com/file/d/1iq-qsmOZ4svLA6dfUPBe7b-pNP5wHHqk/view?usp=sharing" target="_blank" rel="noreferrer">
+                <a href="https://drive.google.com/file/d/15ANhcdz0P_Dz33gFBl7gSYvPNGwgsCuQ/view?usp=sharing" target="_blank" rel="noreferrer">
                     <button className="bg-[#001b5e] text-gray-100 mt-4 w-full md:w-auto md:px-8 p-4 rounded-lg">
                         Download CV
                     </button>
