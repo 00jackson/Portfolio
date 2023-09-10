ed5b4e1663d33f05075a7ceb5b5e2bc289ee1723
Jackson Kujur <95626883+00jackson@users.noreply.github.com>
2023-09-10 22:14:15 +0530

    Update Sidenav.jsx

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -21,6 +21,11 @@ const Sidenav = () => {
                             <AiOutlineHome size={20} />
                             <span className='pl-4'>Home</span>
                         </a>
+                        <a onClick={handleNav} href="#main"
+                            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                            <GrProjects size={20} />
+                            <span className='pl-4'>Work</span>
+                        </a>
                         <a onClick={handleNav} href="#projects"
                             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                             <AiOutlineProject size={20} />
@@ -47,6 +52,9 @@ const Sidenav = () => {
                 <div className='flex flex-col'>
                     <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <AiOutlineHome size={20} />
+                    </a>
+                    <a href="#work" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <GrProjects size={20} />
                     </a>
                     <a href="#projects" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-300'>
                         <AiOutlineProject size={20} />
